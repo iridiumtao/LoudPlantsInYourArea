8115abe3f38d660fdec51870fa7741f324494623
iridiumtao <iridiumtao@gmail.com>
2025-04-21 13:00:12 -0400

    Initial Commit

---
+++ LaudPlantsTests/LaudPlantsTests.swift
@@ -0,0 +1,17 @@
+//
+//  LaudPlantsTests.swift
+//  LaudPlantsTests
+//
+//  Created by 歐東 on 4/21/25.
+//
+
+import Testing
+@testable import LaudPlants
+
+struct LaudPlantsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
