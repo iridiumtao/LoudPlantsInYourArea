22819678d79398830e6c9d1c44e9f552bde849f2
iridiumtao <iridiumtao@gmail.com>
2025-05-15 14:27:22 -0400

    organized AR models

--- LoudPlants/ViewModels/DemoViewModel.swift
+++ LoudPlants/ViewModels/DemoViewModel.swift
@@ -19,24 +19,40 @@ class DemoViewModel: ObservableObject {
 
     init() {
         // define models
-        // Define all PlantEntitys
+        // Define all PlantEntitis
+        let greenDotOffset = SIMD3<Float>(0.048, 0.296, -0.028)
+        let greenDotSize: Float = 0.3
+
         let plantEntities: [PlantEntity] = [
             PlantEntity(
                 id: "1",
-                modelName: "Flytrap",
+                modelName: "Flytrap with Green Dot",
                 thumbnailName: "flytrap"
+                // no greenDot because modelName contains "Green Dot"
             ),
             PlantEntity(
                 id: "2",
-                modelName: "Sad Plant",
-                thumbnailName: "flytrap_crying",
-                greenDot: GreenDot(offset: SIMD3<Float>(0.048, 0.296, -0.028), size: 0.3)
+                modelName: "Flytrap",
+                thumbnailName: "flytrap",
+                greenDot: GreenDot(offset: SIMD3<Float>(0.048, 0.296, -0.028), size: 0.3) // only Flytrap gets a green dot
             ),
+
             PlantEntity(
                 id: "3",
-                modelName: "Happy Plant",
+                modelName: "Succulent",
                 thumbnailName: "succulent",
-                greenDot: GreenDot(offset: SIMD3<Float>(0.048, 0.296, -0.028), size: 0.3)
+                greenDot: GreenDot(offset: SIMD3<Float>(0.048, 0.596, -0.028), size: 0.3) // succulent without background also gets a green dot
+            ),
+
+            PlantEntity(
+                id: "5",
+                modelName: "Happy Plant WoZ", // hidden plant
+                thumbnailName: "succulent",
+            ),
+            PlantEntity(
+                id: "6",
+                modelName: "Sad Plant WoZ", // hidden plant
+                thumbnailName: "flytrap",
             )
         ]
         self.plantEntities = plantEntities
@@ -48,7 +64,7 @@ class DemoViewModel: ObservableObject {
                 name: "Fake Stark",
                 plantSpecies: "Venus Flytrap",
                 status: .sad,
-                entity: plantEntities.first(where: { $0.id == "1" })!,
+                entity: plantEntities.first(where: { $0.modelName == "Flytrap with Green Dot" })!,
                 imageName: "flytrap",
                 overlaySize: (x: 0.9, y: 0.4)
             ),
@@ -56,8 +72,8 @@ class DemoViewModel: ObservableObject {
                 id: "2",
                 name: "Stark",
                 plantSpecies: "Venus Flytrap",
-                status: .crying,
-                entity: plantEntities.first(where: { $0.id == "2" })!,
+                status: .happy,
+                entity: plantEntities.first(where: { $0.modelName == "Flytrap" })!,
                 imageName: "flytrap",
                 overlaySize: (x: 0.9, y: 0.4)
             ),
@@ -66,10 +82,28 @@ class DemoViewModel: ObservableObject {
                 name: "Peter",
                 plantSpecies: "Succulents",
                 status: .happy,
-                entity: plantEntities.first(where: { $0.id == "3" })!,
+                entity: plantEntities.first(where: { $0.modelName == "Succulent" })!,
                 imageName: "succulent",
                 overlaySize: (x: 0.9, y: 0.4)
-            )
+            ),
+            Plant(
+                id: "5",
+                name: "Happy Peter",
+                plantSpecies: "succulent",
+                status: .happy,
+                entity: plantEntities.first(where: { $0.modelName == "Happy Plant WoZ" })!,
+                imageName: "succulent",
+                overlaySize: (x: 0.9, y: 0.4)
+            ),
+            Plant(
+                id: "6",
+                name: "Sad Stark",
+                plantSpecies: "Venus Flytrap",
+                status: .sad,
+                entity: plantEntities.first(where: { $0.modelName == "Sad Plant WoZ" })!,
+                imageName: "flytrap",
+                overlaySize: (x: 0.9, y: 0.4)
+            ),
         ]
         
         self.plantStore = PlantStore(initialPlants: plants)
